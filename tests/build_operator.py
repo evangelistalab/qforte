43491060216df9592724ff03f5346ed47ae07a90
Nan <foreceed@hotmail.com>
2019-02-28 17:46:39 -0500

    Fix the builder test case

--- tests/build_operator.py
+++ tests/build_operator.py
@@ -1,34 +1,41 @@
+import unittest
 import qforte
 from openfermion.ops import QubitOperator
 #from openfermion.ops import QubitOperator
 
-trial_state = qforte.QuantumComputer(4)
+class BuilderTests(unittest.TestCase):
+    def test_build_from_openfermion(self):
+        trial_state = qforte.QuantumComputer(4)
+        
+        trial_prep = [None]*5
+        trial_prep[0] = qforte.make_gate('H',0,0)
+        trial_prep[1] = qforte.make_gate('H',1,1)
+        trial_prep[2] = qforte.make_gate('H',2,2)
+        trial_prep[3] = qforte.make_gate('H',3,3)
+        trial_prep[4] = qforte.make_gate('cX',0,1)
+        
+        trial_circ = qforte.QuantumCircuit()
+        
+        #prepare the circuit
+        for gate in trial_prep:
+            trial_circ.add_gate(gate)
+        
+        # use circuit to prepare trial state
+        trial_state.apply_circuit(trial_circ)
+        
+        test_operator = QubitOperator('X2 Y1', 0.0-0.25j)
+        test_operator += QubitOperator('Y2 Y1', 0.25)
+        test_operator += QubitOperator('X2 X1', 0.25)
+        test_operator += QubitOperator('Y2 X1', 0.0+0.25j)
+        print(test_operator)
+        
+        qforte_operator = qforte.build_from_openfermion(test_operator)
+        
+        qforte.smart_print(qforte_operator)
+        
+        exp = trial_state.direct_op_exp_val(qforte_operator)
+        print(exp)
+        self.assertAlmostEqual(exp, 0.2499999999999999 + 0.0j)
 
-trial_prep = [None]*5
-trial_prep[0] = qforte.make_gate('H',0,0)
-trial_prep[1] = qforte.make_gate('H',1,1)
-trial_prep[2] = qforte.make_gate('H',2,2)
-trial_prep[3] = qforte.make_gate('H',3,3)
-trial_prep[4] = qforte.make_gate('cX',0,1)
-
-trial_circ = qforte.QuantumCircuit()
-
-#prepare the circuit
-for gate in trial_prep:
-    trial_circ.add_gate(gate)
-
-# use circuit to prepare trial state
-trial_state.apply_circuit(trial_circ)
-
-test_operator = QubitOperator('X2 Y1', 0.0-0.25j)
-test_operator += QubitOperator('Y2 Y1', 0.25)
-test_operator += QubitOperator('X2 X1', 0.25)
-test_operator += QubitOperator('Y2 X1', 0.0+0.25j)
-print(test_operator)
-
-qforte_operator = qforte.build_from_openferm(test_operator)
-
-qforte.smart_print(qforte_operator)
-
-exp = trial_state.direct_op_exp_val(qforte_operator)
-print(exp)
+if __name__ == '__main__':
+    unittest.main()
