ed8647b84f221b02724cc0ddb959eeaf849ee0d7
Nan <foreceed@hotmail.com>
2019-03-04 16:42:53 -0500

    -Remove the openfermion-dependent test cases -Modify the print

--- src/qforte/helper/io_helper.py
+++ src/qforte/helper/io_helper.py
@@ -1,6 +1,6 @@
 import qforte
 
-def smart_print(Inputobj):
+def smart_print(Inputobj, print_type='compact'):
 
     """
     smart_print is a function that formatted and print instances of 
@@ -8,20 +8,64 @@ def smart_print(Inputobj):
     QuantumnComputer
 
     :param Inputobj: the input instance one want to print.
+    :param print_type: print format, full or compact
     """
     
     if isinstance(Inputobj, qforte.QuantumOperator):
-        print('\n Quantum operator:')
-        ops_term = Inputobj.terms()
-        for term in ops_term:
-            print('\n')
-            print(term[0])
-            print('\n'.join(term[1].str()))
+        print('\n Quantum operator:')a
+
+        if print_type == 'full':
+            ops_term = Inputobj.terms()
+            for term in ops_term:
+                print(term[0])
+                print('\n'.join(term[1].str()))
+                print('\n')
+
+        if print_type == 'compact':
+            ops_term = Inputobj.terms()
+            for term in ops_term:
+                print(term[0], end="")
+                strp = term[1].str()
+                print('[', end="")
+                for termstr in strp:
+                    tmpstr = termstr.split('\n')
+                    print(tmpstr[0][0], end="")
+                    print(tmpstr[0][21], end=" ")
+                print(']')
 
     if isinstance(Inputobj, qforte.QuantumCircuit):
         print('\n Quantum circuit:')
-        print('\n'.join(Inputobj.str()))
+
+        if print_type == 'full':
+            print('\n'.join(Inputobj.str()))
+
+        if print_type == 'compact':
+                strp = Inputobj.str()
+                print('[', end="")
+                for termstr in strp:
+                    tmpstr = termstr.split('\n')
+                    print(tmpstr[0][0], end="")
+                    print(tmpstr[0][21], end=" ")
+                print(']')
 
     if isinstance(Inputobj, qforte.QuantumComputer):
         print('\n Quantum Computer:')
         print('\n'.join(Inputobj.str()))
+
+def build_circuit(Inputstr):
+    circ = qforte.QuantumCircuit()
+    sepstr = Inputstr.split()
+
+    for i in lens(sepstr):
+        numstr = ''
+        for j in lens(sepstr)-1:
+            numstr += sepstr[j+1]
+            if sepstr[j+1] == ' ':
+                break
+        
+        circ.add_gate(qforte.make_gate(sepstr[0], numstr))
+    
+    return circ
+           
+#def build_operator(Inputstr):
+
