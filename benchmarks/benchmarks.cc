f48b07b2249c6f343aa2019468556fe0ae2a9254
Francesco Evangelista <francesco.evangelista@gmail.com>
2019-08-02 16:56:03 -0400

    Fix issues with updated catch2

--- benchmarks/benchmarks.cc
+++ benchmarks/benchmarks.cc
@@ -35,19 +35,19 @@ TEST_CASE("QuantumComputer", "[benchmark]") {
     prepare_circ(qcirc_16, 0, 16);
     prepare_circ(qcirc_18, 0, 18);
 
-    BENCHMARK("qc_4_apply_circuit") { qc_4.apply_circuit(qcirc_4); }
+    BENCHMARK("qc_4_apply_circuit") { qc_4.apply_circuit(qcirc_4); };
 
-    BENCHMARK("qc_8_apply_circuit") { qc_8.apply_circuit(qcirc_8); }
+    BENCHMARK("qc_8_apply_circuit") { qc_8.apply_circuit(qcirc_8); };
 
-    BENCHMARK("qc_16_apply_circuit") { qc_16.apply_circuit(qcirc_16); }
+    BENCHMARK("qc_16_apply_circuit") { qc_16.apply_circuit(qcirc_16); };
 
-    BENCHMARK("qc_18_apply_circuit") { qc_18.apply_circuit(qcirc_18); }
+    BENCHMARK("qc_18_apply_circuit") { qc_18.apply_circuit(qcirc_18); };
 
-    BENCHMARK("qc_4_apply_circuit_fast") { qc_4.apply_circuit_fast(qcirc_4); }
+    BENCHMARK("qc_4_apply_circuit_fast") { qc_4.apply_circuit_fast(qcirc_4); };
 
-    BENCHMARK("qc_8_apply_circuit_fast") { qc_8.apply_circuit_fast(qcirc_8); }
+    BENCHMARK("qc_8_apply_circuit_fast") { qc_8.apply_circuit_fast(qcirc_8); };
 
-    BENCHMARK("qc_16_apply_circuit_fast") { qc_16.apply_circuit_fast(qcirc_16); }
+    BENCHMARK("qc_16_apply_circuit_fast") { qc_16.apply_circuit_fast(qcirc_16); };
 
-    BENCHMARK("qc_18_apply_circuit_fast") { qc_18.apply_circuit_fast(qcirc_18); }
+    BENCHMARK("qc_18_apply_circuit_fast") { qc_18.apply_circuit_fast(qcirc_18); };
 }
