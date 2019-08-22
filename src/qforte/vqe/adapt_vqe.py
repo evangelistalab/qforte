ef382e66acd0118eb3afb0ff38170e64d5eea13e
RenkeHuang <renke_huang@outlook.com>
2019-08-22 17:47:31 -0400

    add adapt_vqe module with ADAPT_VQE class(working)

--- src/qforte/vqe/adapt_vqe.py
+++ src/qforte/vqe/adapt_vqe.py
@@ -2,18 +2,13 @@ import openfermion
 from openfermion.ops import FermionOperator
 from openfermion.transforms import jordan_wigner, get_fermion_operator
 from openfermion.utils import hermitian_conjugated, commutator, normal_ordered
-
+from openfermionpsi4 import *
 import numpy as np 
 import scipy
-
 import qforte
-# from qforte.utils.trotterization import *
-from qforte.utils.exponentiate import *
+from qforte.utils.exponentiate import exponentiate_single_term
 
-from openfermionpsi4 import *
-
-
-def runPsi4(geometry, **kwargs):
+def runPsi4(geometry, kwargs):
     """ Returns an updated MolecularData object
 
     Parameters
@@ -38,25 +33,37 @@ def runPsi4(geometry, **kwargs):
                         run_scf=kwargs.get('run_scf', 1),
                         run_mp2=kwargs.get('run_mp2', 0),
                         run_cisd=kwargs.get('run_cisd', 0),
-                        run_ccsd=kwargs.get('run_ccsd', 1),
-                        run_fci=kwargs.get('run_fci', 1))
+                        run_ccsd=kwargs.get('run_ccsd', 0),
+                        run_fci=kwargs.get('run_fci', 0))
     return molecule
 
 
 class ADAPT_VQE:
-    """
+    """ ADAPT_VQE class is initiaized with an instance of openfermion.hamiltonians.MolecularData
+
     Attributes
     ----------
     molecule: instance of openfermion.hamiltonians.MolecularData class.
+    jw_ops: list of qforte.QuantumOperator instances (A_i)
+            obtained by jordan-wigner encoding all fermionic singles and doubles excitation operators.
+    jw_commutators: list of qforte.QuantumOperator instances (commutator [H, A_i])
 
-    jw_ops: dict
-        keys: A_i, qforte.QuantumOperator instances
-        values: commutator [H, A_i], qforte.QuantumOperator instances
+    occ_idx_alpha: list of indices of qubits encoding occupied alpha spin orbitals (even indices)
+    occ_idx_beta: list of indices of qubits encoding occupied beta spin orbitals (odd indices)
+    vir_idx_alpha: list of indices of qubits encoding virtual alpha spin orbitals (even indices)
+    vir_idx_beta: list of indices of qubits encoding virtual beta spin orbitals (odd indices)
+
+    ref_state: instance of qforte.QuantumComputer class
+    ansatz_ops: list of qforte.QuantumOperator instances which prepare the wavefunction ansatz
+    ansatz_op_idx: list of "jw_ops-indices" of operators in ansatz_ops list
+    energy: list of variational optimized energies in all ADAPT cycles before convergence
+
     """
     def __init__(self, molecule_instance):
         self.molecule = molecule_instance
         self.jw_ops = []
         self.jw_commutators = []
+        self.qubit_hamiltonian, self.h_qubit_OF = self.get_qubit_hamitonian()
 
         self.occ_idx_alpha = [
             2*i for i in range(molecule_instance.get_n_alpha_electrons())]
@@ -67,28 +74,49 @@ class ADAPT_VQE:
         self.vir_idx_beta = [2*a+1 for a in range(
             self.molecule.get_n_beta_electrons(), molecule_instance.n_orbitals)]
         
-        self.ref_state = self.ref_state()
-        # self.param_list = []
+        # self.ref_state = self.ref_state()
         self.ansatz_ops = []
         self.ansatz_op_idx = []
+        self.energy = []
 
+    # @property
+    # def qubit_hamiltonian(self):
+    #     return self._qubit_hamiltonian
 
-    def build_operator_pool(self, docc_indices=None, active_orb_indices=None):
+    # @qubit_hamiltonian.setter
+    def get_qubit_hamitonian(self, docc_indices=None, active_orb_indices=None):
+        """ Return Hamiltonian operator as a qforte.QuantumOperator instance
+
+        Parameters
+        ----------
+        docc_indices: list, optional
+            list of spatial orbital indices indicating which orbitals should be considered doubly occupied.
+        active_orb_indices: list, optional
+            list of spatial orbital indices indicating which orbitals should be considered active.
+
+        """
+        # "molecular_hamiltonian": instance of the MolecularOperator class
+        
         molecular_hamiltonian = self.molecule.get_molecular_hamiltonian(
-            occupied_indices=Nodocc_indices, active_indices=active_orb_indices)
+            occupied_indices=docc_indices, active_indices=active_orb_indices)
         h_fermion = normal_ordered(get_fermion_operator(molecular_hamiltonian))
-        h_qubit = jordan_wigner(h_fermion)
+        h_qubit_OF = jordan_wigner(h_fermion)
+        h_qubit_qf = qforte.build_from_openfermion(h_qubit_OF)
+        return h_qubit_qf, h_qubit_OF 
 
+    def build_operator_pool(self):
+        """ Call this function to populate jw_ops list and jw_commutators list
+        """
         # Singles
         def add_singles(occ_idx, vir_idx):
-            for i in occ_idx_alpha:
-                for a in vir_idx_alpha:
+            for i in occ_idx:
+                for a in vir_idx:
                     single = FermionOperator(((a, 1), (i, 0)))
                     # 1. build Fermion anti-Hermitian operator
                     single -= hermitian_conjugated(single)
                     # 2. JW transformation to qubit operator
                     jw_single = jordan_wigner(single)
-                    h_single_commutator = commutator(h_qubit, jw_single)
+                    h_single_commutator = commutator(self.h_qubit_OF, jw_single)
                     # 3. qforte.build_from_openfermion(OF_qubitop)
                     qf_jw_single = qforte.build_from_openfermion(jw_single)
                     qf_commutator = qforte.build_from_openfermion(h_single_commutator)
@@ -109,7 +137,7 @@ class ADAPT_VQE:
                     double = FermionOperator(F'{a}^ {b}^ {i} {j}')
                     double -= hermitian_conjugated(double)
                     jw_double = jordan_wigner(double)
-                    h_double_commutator = commutator(h_qubit, jw_double)
+                    h_double_commutator = commutator(self.h_qubit_OF, jw_double)
 
                     qf_jw_double = qforte.build_from_openfermion(jw_double)
                     qf_commutator = qforte.build_from_openfermion(
@@ -128,22 +156,22 @@ class ADAPT_VQE:
         vir_b_pairs = list(combinations(self.vir_idx_beta, 2))
         add_doubles(occ_b_pairs, vir_b_pairs)
 
-        occ_ab_pairs = list(product(self.occ_idx_alpha, occ_idx_beta))
-        vir_ab_pairs = list(product(self.vir_idx_alpha, vir_idx_beta))
+        occ_ab_pairs = list(product(self.occ_idx_alpha, self.occ_idx_beta))
+        vir_ab_pairs = list(product(self.vir_idx_alpha, self.vir_idx_beta))
         add_doubles(occ_ab_pairs, vir_ab_pairs)
 
-
+    @property
     def ref_state(self):
         """ Return a qforte.QuantumComputer instance which represents the Hartree-Fock state of the input molecule
         """
         hf_qc = qforte.QuantumComputer(self.molecule.n_qubits)
         hf_cir = qforte.QuantumCircuit()
 
-        for i_a in occ_idx_alpha:
+        for i_a in self.occ_idx_alpha:
             X_ia = qforte.make_gate('X', i_a, i_a)
             hf_cir.add_gate(X_ia)
         
-        for i_b in occ_idx_beta:
+        for i_b in self.occ_idx_beta:
             X_ib = qforte.make_gate('X', i_b, i_b)
             hf_cir.add_gate(X_ib)
         
@@ -152,17 +180,23 @@ class ADAPT_VQE:
 
     def get_ansatz_circuit(self, param_list):
         """ Return a qforte.QuantumCircuit object parametrized by input param_list
+        
         Parameters
         ----------
         param_list: list
-            list of parameters (rotation angles) used for preparing circuit applied on the reference state 
-            (NOT the circuit to prepare reference state)
+            list of parameters [param_1,..., param_n]
+            to prepare the circuit 'exp(param_n*A_n)...exp(param_1*A_1)'
+        
+        Returns
+        -------
+        param_circuit: instance of qforte.QuantumCircuit class
+            the circuit to be applied on the reference state to get wavefunction ansatz. 
+        
         """
         param_circuit = qforte.QuantumCircuit()
         for i in range(len(param_list)):
             param_i = param_list[i]
-            idx, param = param_list[i]
-            op = self.jw_ops[i]
+            op = self.ansatz_ops[i]
             # exp_op is a circuit object
             exp_op = qforte.QuantumCircuit()
 
@@ -175,37 +209,47 @@ class ADAPT_VQE:
             param_circuit.add_circuit(exp_op)
         return param_circuit
 
-    def compute_gradient(self, param_list, use_analytic_grad=True, numerical_grad_precision=1e-3):
+    def compute_gradient(self, param_list, use_analytic_grad=True, numerical_grad_precision=1e-3, gvec_abs = True):
         """ Return a list of energy gradients for all operators in jw_ops list
 
         Parameters
         ----------
-        phi_i: qforte.QuantumComputer instance
-            quantum state / trial wavefunction at step i
+        param_list: list
+            list of parameters [param_1,..., param_n]
+            to prepare the circuit 'exp(param_n*A_n)...exp(param_1*A_1)'
+        use_analytic_grad: boolean
 
         Returns
         -------
         gradients: list
-            energy gradient w.r.t. param_i
+            list of energy gradients (Absolute Value) w.r.t. all param_i in the jw_ops list
         """
         gradients = []
-        if param_list == None:
-            phi_i = self.ref_state
-        else: 
-            param_circuit = self.get_ansatz_circuit(param_list)
-            phi_i = self.ref_state.apply_circuit(param_circuit)
+
+        param_circuit = self.get_ansatz_circuit(param_list)
+        current_wfn = self.ref_state
+        current_wfn.apply_circuit(param_circuit)
 
         if use_analytic_grad:
-            for op in self.jw_ops:
-                commutator = self.jw_commutators[self.jw_ops.index(op)]
-                term_sum = 0.0
-                n_terms = len(commutator.terms())
 
-                for i in range(n_terms):
-                    term_sum += commutator.terms()[i][0] * \
-                        phi_i.perfect_measure_circuit(commutator.terms()[i][1])
-                
-                term_sum = np.real(term_sum)
+            for i in range(len(self.jw_ops)):
+                commutator = self.jw_commutators[i]
+                term_sum = 0.0
+
+                ##Use inner product as measurement
+                # dot = current_wfn.direct_op_exp_val(commutator)
+                # dot = np.real(dot)
+                # gradients.append(dot)
+                for term_i in commutator.terms():
+                    term_sum += term_i[0] * current_wfn.perfect_measure_circuit(term_i[1])
+                # n_terms = len(commutator.terms())
+                # for i in range(n_terms):
+                #     term_sum += commutator.terms()[i][0] * \
+                #         current_wfn.perfect_measure_circuit(commutator.terms()[i][1])
+                if gvec_abs:
+                    term_sum = np.abs(np.real(term_sum))
+                else: 
+                    term_sum = np.real(term_sum)
                 gradients.append(term_sum)
         else:
             # use numerical gradients 
@@ -223,81 +267,117 @@ class ADAPT_VQE:
 
             #     gradient_i = (E_i_plus - E_i_minus) /(2*numerical_grad_precision)
             #     gradients.append(gradient_i)
-            pass
+            print('Numerical gradient not implemented yet.')
 
 
         return gradients
 
 
-    # def compute_expectation(self, param_list, ref_state):
-    #     """ Return energy 
-    #     """
-    #     param_circuit = self.get_ansatz_circuit(param_list)
-    #     energy = ref_state.perfect_measure_circuit(param_circuit)
-    #     return energy
-
     def compute_expectation(self, param_list):
-        """ Return <phi_i|H|phi_i>
+        """ Return <wfn|H|wfn>
+
+        Parameters
+        ----------
+        param_list: list
+            list of parameters [param_1,..., param_n]
+            to prepare the circuit 'exp(param_n*A_n)...exp(param_1*A_1)'
+
+        Returns
+        -------
+        expectation: float
+            <HF|exp(-param_1*A_1)...exp(-param_n*A_n)*H*exp(param_n*A_n)...exp(param_1*A_1)|HF>
         """
         param_circuit = self.get_ansatz_circuit(param_list)
-        expectation = self.ref_state.perfect_measure_circuit(param_circuit)
+        current_wfn = self.ref_state
+        current_wfn.apply_circuit(param_circuit)
+
+        expectation = 0.0
+        for h_i in self.qubit_hamiltonian.terms():
+            expectation += h_i[0] * current_wfn.perfect_measure_circuit(h_i[1])
+
+        expectation = np.real(expectation)
         return expectation
 
-    def get_variational_energy(input_params):
-        result = scipy.optimize.minimize(compute_expectation, input_params, method= ‘BFGS’,\
-                jac=Trotter_Gradient, \
-                options={‘gtol’: float(theta_tightness), ‘disp’: False}, callback=Callback)
+    def variational_optimizer(self, input_params):
+        """ Return scipy.optimize.OptimizeResult object
 
-    # lambda H, state: 
-    def iterating_cycle(self, max_cycle = 100, gradient_norm_threshold = 1e-3):
-        # 3. initialize |HF> state, select op1 from jw_ops list
-        phi0 = self.ref_state
+        Parameters
+        ----------
+        input_params: list
+            list of paramaters which prepares the wavefunction ansatz
 
-        params = []
-        # for i in range(len(jw_ops_list)):
-        #     gradients_i = 
-        gradients_0 = self.compute_gradient(phi0)
-        idx_max_grad = gradients_0.index(max(gradients_0))
-        A_1 = jw_ops_list[idx_max_grad]
-        param_1 = 0.0
-        params.append((idx_max_grad, param_1))
-        
-       
+        Returns
+        -------
+        result: a scipy.optimize.OptimizeResult object with important attributes:
+                - result.x: (ndarray) optimized parameters
+                - result.success: (bool) whether or not the optimizer exited successfully
+                - result.fun: (float) minimized expectation value (energy)
 
-        cir_trot, phase = trotterize(operator)
-        # 4. prepare state |phi1> = exp(op1*param1)|HF>
-        # 5. gradients measure   
-        #    Input: list of op;     Output: list of gradient values
-        # 6. select op2 with largest gradient, prepare state |phi2> = exp(op2*param2)|phi1>
-        #  minimize E = <phi2|H|phi2>
-        
+                - result.message: (string) description of the cause of the termination
+        """
+        print('        ---------------- Start VQE ----------------')
+        print('        Optimizing parameters to minimize energy...')
+        # jac = compute_gradient
+        # result = scipy.optimize.minimize(self.compute_expectation, input_params, method='BFGS', options={'gtol': 1e-2})
+        input_param_array = np.asarray(input_params)
+        result = scipy.optimize.minimize(
+            self.compute_expectation, input_param_array, method='BFGS')
+
+        print('        '+str(len(input_params)) +
+              ' parameters optimized after ' + str(result.nit) + ' iterations')
+        print('        ---------------- Finish VQE ----------------')
+        return result
+
+    def iterating_cycle(self, max_cycle= 100, gradient_norm_threshold = 1e-2, print_details = True, restart = False):
+        if not restart:
+            print('\n    ================> Start ADAPT-VQE <================\n')
+            params = []
+        else:
+            print('\n    ================> reStart ADAPT-VQE <================\n')
+            print(F'    NOT support restart yet.')
 
 
-        pass
-        
-        
+        for i in range(1, max_cycle+1):
+            print(F'    ====== ADAPT cycle {i} ======')
+            gradients_i = self.compute_gradient(params)
+            # print(F'    gradient_vector = {gradients_i}')
+            if np.linalg.norm(gradients_i) < gradient_norm_threshold:
+                print('\n    =========> Finish ADAPT-VQE Successfully! <=========')
+                print(F'    Norm of gradient vector Converged! (norm less than {gradient_norm_threshold})\n')
+                if print_details:
+                    print('\n--- Details ---')
+                    print('#op in op pool = ', len(self.jw_ops))
+                    print('#commutator = ', len(self.jw_commutators))
+                    print('#ansatzOp = ', len(self.ansatz_ops))
+                    print('#ansatzOpIdx = ', len(self.ansatz_op_idx))
+                    print('Energy from 1st ADAPT iteration: {} Hartree'.format(self.energy[0]))
+                    print('Energy from final ADAPT iteration: {} Hartree'.format(self.energy[-1]))
+                    print('\n--- reference ---')
+                    print('psi4 HF  energy: {} Hartree.'.format(self.molecule.hf_energy))
+                    print('psi4 FCI energy: {} Hartree.'.format(self.molecule.fci_energy))
+                break
+                
+            else:
+                idx_of_max_grad_i = gradients_i.index(max(gradients_i))
+                self.ansatz_op_idx.append(idx_of_max_grad_i)
+                print(F'    ansatz_op_idx = {self.ansatz_op_idx}')
+
+                A_i = self.jw_ops[idx_of_max_grad_i]
+                self.ansatz_ops.append(A_i)
+                # print(F'    ansatz_ops = {self.ansatz_ops}')
+
+                param_i = 0.005
+                params = np.append(params, param_i)
+                # print(F'    Current parameters = {params}')
+
+                result = self.variational_optimizer(params)
+                params = result.x
+                # print(F'    Optimized parameters = {params}')
+                energy_i = result.fun
+                self.energy.append(energy_i)
+                print(F'    Optimized Energy of ADAPT cycle {i}: {energy_i}\n')
+        else: 
+            print(F'Warning: Norm of gradients vector did not converge (less than {gradient_norm_threshold}) in {max_cycle} cycles')
 
 
-
-
-
-
-# def vqe_minimizer(initial_params):
-#     """ Return an updated qforte QuantumComputer object
     
-#     Parameters
-#     ----------
-#     initial_params: list
-#         List of parameters which define the ansatz
-#     ansatz: string
-#         'ucc', 
-
-#     Returns
-#     -------
-#     updated_quantum_computer: qforte.QuantumComputer object
-#         QuantumComputer object which parametrized using new list of parameters
-#     """
-
-
-#     return updated_quantum_computer
-#     pass
--- tests/test_adaptvqe.py
+++ tests/test_adaptvqe.py
@@ -2,26 +2,15 @@ import qforte
 from qforte.vqe.adapt_vqe import *
 
 import pytest
-import sys
 import numpy as np
-import numpy.testing as npt
-from timeit import default_timer as timer
 
-def test_build_operator_pool():
-    geom = [('H', (0, 0, 0)), ('H', (0, 0, 0.7414))]
-    # set_up = {basis, 
-    # multiplicity, 
-    # charge, 
-    # description, 
-    # run_scf, 
-    # run_mp2, 
-    # run_cisd, 
-    # run_ccsd, 
-    # run_fci }
-    mol = runPsi4(geom)
-    adapt_obj = ADAPT_VQE(mol)
-    adapt_obj.build_operator_pool()
-    adapt_ins.jw_ops
+
+
+
+def test_adapt_vqe():
+
+
+
 
 
 
