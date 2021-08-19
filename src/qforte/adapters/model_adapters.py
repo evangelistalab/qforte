5b90d575c10b0fd48178d44f73d15917aa7aa8b1
Jonathon Misiewicz <jonathon.paul.misiewicz@emory.edu>
2021-08-19 09:12:46 -0400

    First step towards spin system

---
+++ src/qforte/adapters/model_adapters.py
@@ -0,0 +1,20 @@
+import qforte as qf
+from qforte.system.molecular_info import System
+
+def create_TFIM(n: int, h: float, J: float, closed = False):
+    """Creates a Transverse Field Ising Model hamiltonian with
+    open boundary conditions, i.e., no interaction between the
+    first and last spin sites."""
+
+    TFIM = System()
+    TFIM.hamiltonian = qf.QubitOperator()
+
+    circuit = [(-h, f"Z_{i}") for i in range(n)]
+    circuit += [(-J, f"X_{i} X_{i+1}") for i in range(n-1)]
+
+    for coeff, op_str in circuit:
+        TFIM.hamiltonian.add(coeff, qf.build_circuit(op_str))
+
+    TFIM.hf_reference = [0] * n
+
+    return TFIM
