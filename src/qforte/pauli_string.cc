8a6c46a8ebf3a463796bc0d3060db6a0dd062f87
imagoulas <magouals@chemistry.msu.edu>
2021-11-23 11:02:16 -0500

    Optimized phase calculation in pauli_string.

--- src/qforte/pauli_string.cc
+++ src/qforte/pauli_string.cc
@@ -26,27 +26,27 @@ bool operator==(const PauliString& lhs, const PauliString& rhs){
 
 std::pair<std::complex<double>,PauliString>
 multiply(const PauliString& lhs, const PauliString& rhs){
-    const std::complex<double> onei(0.0, 1.0);
+    constexpr std::complex<double> i_powers[] = {{1.0, 0.0}, {0.0, 1.0}, {-1.0, 0.0}, {0.0, -1.0}};
     std::complex<double> phase = 1.0;
     uint8_t imaginary_phase = 0;
     uint8_t minus_one_phase = 0;
-    const uint64_t rhsX_lhsZ           =                     (rhs.X() & lhs.Z()).get_bits();
-    const uint64_t rhsZ_lhsX           =                     (rhs.Z() & lhs.X()).get_bits();
-    const uint64_t rhsX_lhsX_lhsZ      =           (rhs.X() & lhs.X() & lhs.Z()).get_bits();
-    const uint64_t rhsX_rhsZ_lhsZ      =           (rhs.X() & rhs.Z() & lhs.Z()).get_bits();
-    const uint64_t rhsX_rhsZ_lhsX      =           (rhs.X() & rhs.Z() & lhs.X()).get_bits();
-    const uint64_t rhsZ_lhsX_lhsZ      =           (rhs.Z() & lhs.X() & lhs.Z()).get_bits();
-    const uint64_t rhsX_rhsZ_lhsX_lhsZ = (rhs.X() & rhs.Z() & lhs.X() & lhs.Z()).get_bits();
+    const uint64_t rhsX_lhsZ           =                     ui64_bit_count((rhs.X() & lhs.Z()).get_bits());
+    const uint64_t rhsZ_lhsX           =                     ui64_bit_count((rhs.Z() & lhs.X()).get_bits());
+    const uint64_t rhsX_lhsX_lhsZ      =           ui64_bit_count((rhs.X() & lhs.X() & lhs.Z()).get_bits());
+    const uint64_t rhsX_rhsZ_lhsZ      =           ui64_bit_count((rhs.X() & rhs.Z() & lhs.Z()).get_bits());
+    const uint64_t rhsX_rhsZ_lhsX      =           ui64_bit_count((rhs.X() & rhs.Z() & lhs.X()).get_bits());
+    const uint64_t rhsZ_lhsX_lhsZ      =           ui64_bit_count((rhs.Z() & lhs.X() & lhs.Z()).get_bits());
+    const uint64_t rhsX_rhsZ_lhsX_lhsZ = ui64_bit_count((rhs.X() & rhs.Z() & lhs.X() & lhs.Z()).get_bits());
     PauliString ps(lhs.X() ^ rhs.X(),lhs.Z() ^ rhs.Z());
-    imaginary_phase += (        ui64_bit_count(rhsX_lhsZ)
-                        +       ui64_bit_count(rhsZ_lhsX)
-                        - 2.0 * ui64_bit_count(rhsX_rhsZ_lhsX_lhsZ));
-    minus_one_phase += (        ui64_bit_count(rhsX_lhsZ)
-                        -       ui64_bit_count(rhsX_lhsX_lhsZ)
-                        -       ui64_bit_count(rhsX_rhsZ_lhsZ)
-                        +       ui64_bit_count(rhsX_rhsZ_lhsX)
-                        +       ui64_bit_count(rhsZ_lhsX_lhsZ)
-                        -       ui64_bit_count(rhsX_rhsZ_lhsX_lhsZ));
-    phase *= pow(-1.0, minus_one_phase) * pow(onei, imaginary_phase);
+    imaginary_phase += (        rhsX_lhsZ
+                        +       rhsZ_lhsX
+                        - 2.0 * rhsX_rhsZ_lhsX_lhsZ);
+    minus_one_phase += (        rhsX_lhsZ
+                        -       rhsX_lhsX_lhsZ
+                        -       rhsX_rhsZ_lhsZ
+                        +       rhsX_rhsZ_lhsX
+                        +       rhsZ_lhsX_lhsZ
+                        -       rhsX_rhsZ_lhsX_lhsZ);
+    phase *= (1.0 - 2.0*(minus_one_phase & 1)) * i_powers[imaginary_phase & 3];
     return std::make_pair(phase,ps);
 }
