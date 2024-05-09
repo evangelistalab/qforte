gradient structure fixed, need to fix grads themselves

src/qforte/abc/uccvqeabc.py
@@ -199,7 +199,7 @@ class UCCVQE(VQE, UCC):
         else:
             Kmus = []
             Umus = []
-            grads = np.zeros((len(self._Uprep), len(self._pool_obj)))
+            grads = np.zeros((len(self._Uprep), len(self._tops)))
             for mu in range(0, M):
                 if self._compact_excitations:
                     if params is None:
@@ -279,12 +279,11 @@ class UCCVQE(VQE, UCC):
                     qc_psi.set_coeff_vec(psi_i)
                     Kmu_prev = Kmu
 
-            if return_individual == True:
-                np.testing.assert_allclose(np.imag(grads), np.zeros_like(grads), atol=1e-7)
-                return grads
-            else:
-                grads = np.einsum('iu->u', grads)   
-
+        if return_individual == True:
+            np.testing.assert_allclose(np.imag(grads), np.zeros_like(grads), atol=1e-7)
+            return grads
+        else:
+            grads = np.einsum('iu->u', grads)
         np.testing.assert_allclose(np.imag(grads), np.zeros_like(grads), atol=1e-12)
         
         return grads

src/qforte/ucc/gibbsadapt.py
@@ -82,9 +82,9 @@ class Gibbs_ADAPT(UCCVQE):
         U = self.build_Uvqc(amplitudes = x)
         Es, A, ops = qf.ritz_eigh(self._nqb, self._qb_ham, U, [], verbose = False)
         
-        E_grads = self.measure_gradient(params = x, return_individual=True).T
+        E_grads = self.measure_gradient(params = x, return_individual=True)
         
-        E_grads = E_grads@A
+        E_grads = A.T@E_grads
         
 
         Es = np.array(Es)
@@ -121,8 +121,8 @@ class Gibbs_ADAPT(UCCVQE):
         #dF += np.einsum('i,iu->u', omega, d_deltas)
         #dF -= (np.log(tot_gamma)/self.beta)*np.einsum('iu->u', d_omega)
         dF -= (1/(self.beta*tot_gamma))*np.einsum('i,u->u', omega, tot_d_gamma)
-        print("dF shape")
-        print(dF.shape)
+        print(dF)
+        exit() 
         return dF
 
     def compute_addition_gradient(self):
@@ -131,9 +131,9 @@ class Gibbs_ADAPT(UCCVQE):
         
         U = self.build_Uvqc(amplitudes = self._tamps)
         Es, A, ops = qf.ritz_eigh(self._nqb, self._qb_ham, U, [], verbose = False)
-        E_grads = self.measure_gradient3(return_individual = True).T
+        E_grads = self.measure_gradient3(return_individual = True)
         
-        E_grads = E_grads@A
+        E_grads = A.T@E_grads
         #Es = [] 
         #E_grads = []
         #for i, reference in enumerate(self._references):