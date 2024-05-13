8345d138697926932608f3919d4e75760f861b4f
nstair <nick_stair@yahoo.com>
2024-05-13 11:24:42 -0700

    Merged with master (adds apply w/ tensor ham), cleanup.

---
+++ sandbox/davidson/srcd_test_v2.py
@@ -0,0 +1,14 @@
+import qforte
+from qforte import *
+# Define the reference and geometry lists.
+geom = [('Li', (0., 0., 0.0)), ('H', (0., 0., 1.50))]
+
+# Get the molecule object that now contains both the fermionic and qubit Hamiltonians.
+LiHmol = system_factory(build_type='psi4', mol_geometry=geom, basis='sto-3g')
+
+# alg = SRQK(LiHmol, computer_type='fci')
+alg = SRCD(
+    LiHmol, 
+    computer_type='fci')
+
+alg.run()
--- src/qforte/qkd/srcd.py
+++ src/qforte/qkd/srcd.py
@@ -43,8 +43,6 @@ class SRCD(QSD):
 
     """
     def run(self,
-            s=3,
-            dt=0.5,
             target_root=0,
             max_itr=50,
             thresh=1e-9,
@@ -56,9 +54,6 @@ class SRCD(QSD):
 
         self.max_itr = max_itr
         self.thresh = thresh
-        self._s = s
-        self._nstates = s+1
-        self._dt = dt
         self._target_root = target_root
         self._use_exact_evolution = use_exact_evolution
         self._diagonalize_each_step = diagonalize_each_step
@@ -67,12 +62,13 @@ class SRCD(QSD):
         self._n_cnot = 0
         self._n_pauli_trm_measures = 0
 
-        # help(self)
-
         # Print options banner (should done for all algorithms).
         self.print_options_banner()
 
+        self._timer = qforte.local_timer()
+        self._timer.reset()
         self.common_run()
+        self._timer.record("Run")
 
     # Define Algorithm abstract methods.
     def run_realistic(self):
@@ -93,18 +89,8 @@ class SRCD(QSD):
         print('Trial reference state:                   ',  ref_string(self._ref, self._nqb))
         print('Number of Hamiltonian Pauli terms:       ',  self._Nl)
         print('Trial state preparation method:          ',  self._state_prep_type)
-        # print('Trotter order (rho):                     ',  self._trotter_order)
-        # print('Trotter number (m):                      ',  self._trotter_number)
-        # print('Use exact time evolution?:               ',  self._use_exact_evolution)
-        # print('Use fast version of algorithm:           ',  str(self._fast))
-        # if(self._fast):
-        #     print('Measurement varience thresh:             ',  'NA')
-        # else:
-        #     print('Measurement varience thresh:             ',  0.01)
 
         # Specific SRCD options.
-        print('Dimension of Krylov space (N):           ',  self._nstates)
-        # print('Delta t (in a.u.):                       ',  self._dt)
         print('Target root:                             ',  str(self._target_root))
 
 
@@ -120,18 +106,15 @@ class SRCD(QSD):
         print('Number of CNOT gates in deepest circuit:   ', self._n_cnot)
         print('Number of Pauli term measurements:         ', self._n_pauli_trm_measures)
 
+        print("\n\n")
+        print(self._timer)
+
     def build_qk_mats(self):
         return self.build_cd_mats()
 
-    # def build_qk_mats_fast(self):
-
     def build_cd_mats(self):
         if(self._computer_type == 'fock'):
-            # if(self._trotter_order != 1):
             raise ValueError("fock computer SRCD not currently implemented")
-            
-            # return self.build_cd_mats_fock()
-            # return self.build_cd_mats_fci()
         
         elif(self._computer_type == 'fci'):
             if(self._trotter_order not in [1, 2]):
@@ -162,115 +145,18 @@ class SRCD(QSD):
             _nstates by _nstates
         """
 
-        # h_mat = np.zeros((self._nstates,self._nstates), dtype=complex)
-        # s_mat = np.zeros((self._nstates,self._nstates), dtype=complex)
-
-        # Store these vectors for the aid of MRSQK
-        # self._omega_lst = []
-        # Homega_lst = []
-
-        # hermitian_pairs = qforte.SQOpPool()
-
-        # this is updated, evolution time is now just 1.0 here
-        # hermitian_pairs.add_hermitian_pairs(1.0, self._sq_ham)
-
-        # QC = qforte.FCIComputer(
-        #         self._nel, 
-        #         self._2_spin, 
-        #         self._norb)
-            
-        # QC.hartree_fock()
-
         if(self._diagonalize_each_step):
             print('\n\n')
 
-            print(f"{'k(S)':>7}{'E(Npar)':>19}{'N(params)':>14}{'N(CNOT)':>18}{'N(measure)':>20}")
-            print('-------------------------------------------------------------------------------')
+            print(f"{'k(S)':>7}{'E(Npar)':>19}{'dE(Npar)':>19}{'N(params)':>14}")
+            print('--------------------------------------------------------------------------')
 
             if (self._print_summary_file):
                 f = open("summary.dat", "w+", buffering=1)
-                f.write(f"#{'k(S)':>7}{'E(Npar)':>19}{'N(params)':>14}{'N(CNOT)':>18}{'N(measure)':>20}\n")
+                f.write(f"#{'k(S)':>7}{'E(Npar)':>19}{'dE(Npar)':>19}{'N(params)':>14}\n")
                 f.write('#-------------------------------------------------------------------------------\n')
 
-    
-        # """In reviewing this there is going to be an inherent ordering probelm. I want to apply 
-        # based on hermitian paris of SQ operators but the qb hamiltonain has been 'simplified' 
-        # and looses the exact correspondance to the sq hamiltonain"""
-        # for m in range(self._nstates):
-
-        #     if(m>0):
-        #         # Compute U_m |φ>
-        #         if(self._use_exact_evolution):
-        #             QC.evolve_op_taylor(
-        #                 self._sq_ham,
-        #                 self._dt,
-        #                 1.0e-15,
-        #                 30)
-
-        #         else:
-        #             QC.evolve_pool_trotter(
-        #                 hermitian_pairs,
-        #                 self._dt,
-        #                 self._trotter_number,
-        #                 self._trotter_order,
-        #                 antiherm=False,
-        #                 adjoint=False)
-
-        #     C = QC.get_state_deep()
-         
-        #     self._omega_lst.append(C)
-
-        #     QC.apply_sqop(self._sq_ham)
-
-        #     Sig = QC.get_state_deep()
-
-        #     Homega_lst.append(Sig)
-
-        #     # Compute S_mn = <φ| U_m^\dagger U_n |φ> and H_mn = <φ| U_m^\dagger H U_n |φ>
-        #     for n in range(len(self._omega_lst)):
-        #         h_mat[m][n] = self._omega_lst[m].vector_dot(Homega_lst[n])
-        #         h_mat[n][m] = np.conj(h_mat[m][n])
-        #         s_mat[m][n] = self._omega_lst[m].vector_dot(self._omega_lst[n])
-        #         s_mat[n][m] = np.conj(s_mat[m][n])
-
-            # if (self._diagonalize_each_step):
-            #     # TODO (cleanup): have this print to a separate file
-            #     k = m+1
-            #     evals, evecs = canonical_geig_solve(s_mat[0:k, 0:k],
-            #                        h_mat[0:k, 0:k],
-            #                        print_mats=False,
-            #                        sort_ret_vals=True)
-
-            #     scond = np.linalg.cond(s_mat[0:k, 0:k])
-            #     self._n_classical_params = k
-            #     # self._n_cnot = 2 * Um.get_num_cnots()
-            #     self._n_cnot = 0
-            #     self._n_pauli_trm_measures  = k * self._Nl
-            #     self._n_pauli_trm_measures += k * (k-1) * self._Nl
-            #     self._n_pauli_trm_measures += k * (k-1)
-
-                # print(f' {scond:7.2e}    {np.real(evals[self._target_root]):+15.9f}    {self._n_classical_params:8}        {self._n_cnot:10}        {self._n_pauli_trm_measures:12}')
-                # if (self._print_summary_file):
-                #     f.write(f'  {scond:7.2e}    {np.real(evals[self._target_root]):+15.9f}    {self._n_classical_params:8}        {self._n_cnot:10}        {self._n_pauli_trm_measures:12}\n')
-
-        # if (self._diagonalize_each_step and self._print_summary_file):
-        #     f.close()
-
-        # # Some of these doen't matter for classical Davidson
-        # self._n_classical_params = self._nstates
-        # # self._n_cnot = 2 * Um.get_num_cnots()
-        # self._n_cnot = 0
-        # # diagonal terms of Hbar
-        # self._n_pauli_trm_measures  = self._nstates * self._Nl
-        # # off-diagonal of Hbar (<X> and <Y> of Hadamard test)
-        # self._n_pauli_trm_measures += self._nstates*(self._nstates-1) * self._Nl
-        # # off-diagonal of S (<X> and <Y> of Hadamard test)
-        # self._n_pauli_trm_measures += self._nstates*(self._nstates-1)
-
-
-        # return s_mat, h_mat
-
-# Store vectors for subspace construction
+        # Store vectors for subspace construction
         self._omega_lst = []
 
         QC = qforte.FCIComputer(
@@ -301,7 +187,15 @@ class SRCD(QSD):
 
                 QC.set_state(C)
 
-                QC.apply_sqop(self._sq_ham)
+                if(self._apply_ham_as_tensor):
+                    QC.apply_tensor_spat_012bdy(
+                        self._nuclear_repulsion_energy, 
+                        self._mo_oeis, 
+                        self._mo_teis, 
+                        self._mo_teis_einsum, 
+                        self._norb)
+                else:   
+                    QC.apply_sqop(self._sq_ham)
 
                 Sig = QC.get_state_deep()
 
@@ -336,7 +230,15 @@ class SRCD(QSD):
 
             psi_2 = QC.get_state_deep()
 
-            QC.apply_sqop(self._sq_ham)
+            if(self._apply_ham_as_tensor):
+                QC.apply_tensor_spat_012bdy(
+                    self._nuclear_repulsion_energy, 
+                    self._mo_oeis, 
+                    self._mo_teis, 
+                    self._mo_teis_einsum, 
+                    self._norb)
+            else:   
+                QC.apply_sqop(self._sq_ham)
 
             psi_1 = QC.get_state_deep()
 
@@ -376,15 +278,18 @@ class SRCD(QSD):
                 self._n_pauli_trm_measures += k * (k-1) * self._Nl
                 self._n_pauli_trm_measures += k * (k-1)
 
-                print(f' {scond:7.2e}    {lambda_low:15.9f}    {self._n_classical_params:8}        {self._n_cnot:10}        {self._n_pauli_trm_measures:12}')
+                delta_e = np.real(lambda_low - lambda_old)
+
+                print(f' {scond:7.2e}    {np.real(lambda_low):15.9f}    {np.real(delta_e):15.9f}    {self._n_classical_params:8}')
                 # if (self._print_summary_file):
                     # f.write(f'  {scond:7.2e}    {np.real(evals[self._target_root]):+15.9f}    {self._n_classical_params:8}        {self._n_cnot:10}        {self._n_pauli_trm_measures:12}\n')
 
 
             if abs(lambda_old - lambda_low) <= self.thresh:
-                print('converged!')
+                print('\n Davidson Converged!')
+                self._Egs = np.real(np.real(lambda_low))
                 break
 
-        print(f'davidson iteration: {m + 1}, energy: {lambda_low}, energy difference: {lambda_low - lambda_old}')
+        print(f'davidson iteration: {m + 1}, energy: {np.real(lambda_low):15.9f}, energy difference: {lambda_low - lambda_old}')
 
         return s_mat, h_mat
