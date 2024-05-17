d47aaa45c58b553d28c739cfb614ef31c1d200e4
Jonathon Misiewicz <jpmisiewicz@vt.edu>
2024-05-17 16:00:53 -0400

    Addl black

--- src/qforte/abc/mixin.py
+++ src/qforte/abc/mixin.py
@@ -12,12 +12,12 @@ class Trotterizable:
         e^(A + B), the approximate operator C(m) = (e^(A/m) * e^(B/m))^m is
         exact in the infinite m limit.
     """
+
     def __init__(self, *args, trotter_order=1, trotter_number=1, **kwargs):
-       super().__init__(*args, **kwargs)
-       self._trotter_order = trotter_order
-       self._trotter_number = trotter_number 
+        super().__init__(*args, **kwargs)
+        self._trotter_order = trotter_order
+        self._trotter_number = trotter_number
 
     def print_trotter_options(self):
-         print("Trotter order (rho):                     ", self._trotter_order)
-         print("Trotter number (m):                      ", self._trotter_number)
-
+        print("Trotter order (rho):                     ", self._trotter_order)
+        print("Trotter number (m):                      ", self._trotter_number)
