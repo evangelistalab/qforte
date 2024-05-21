f2913c247b07c635961f3fea80a3c4eb68129420
imagoulas <ilias.magoulas@emory.edu>
2024-05-21 13:09:12 -0400

    Added comments, as suggested by Jonathon.

--- src/qforte/abc/ansatz.py
+++ src/qforte/abc/ansatz.py
@@ -32,12 +32,21 @@ class UCC:
         tamps = self._tamps if amplitudes is None else amplitudes
 
         if self._pool_type == "sa_SD":
+        # The elements of the sa_SD pool are typically linear combinations of 
+        # second-quantized, fermionic, anti-Hermitian excitation operators.
+        # To avoid breaking particle number and Sz symmetry, a temporary 
+        # SQOpPool is created where each spin-orbital, anti-Hermitian, 
+        # second-quantized operator is treated as a single element 
+        # (first-order Trotterization).
             for tamp, top in zip(tamps, self._tops):
                 sa_sq_op = self._pool_obj[top][1].terms()
+                # The first half of the terms encode the de-excitation
+                # component of the anti-Hermitian, singlet-spin adapted operator.
                 half_length = len(sa_sq_op) // 2
                 for coeff, cr, ann in sa_sq_op[:half_length]:
                     sq_op = qf.SQOperator()
                     sq_op.add_term(coeff, cr, ann)
+                    # Add the corresponding excitation component.
                     sq_op.add_term(-coeff, ann, cr)
                     temp_pool.add(tamp, sq_op)
         else:
